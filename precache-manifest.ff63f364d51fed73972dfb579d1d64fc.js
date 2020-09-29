785916542c8fdee9ea56da54af95ad99adac8e82
Fatih <kresnofatihimani.private@gmail.com>
2020-09-29 11:53:46 +0700

    Updates

---
+++ precache-manifest.ff63f364d51fed73972dfb579d1d64fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5196da448856a17d598400e318f3fad7",
+    "url": "/covidtrackerappp/index.html"
+  },
+  {
+    "revision": "5dec0d2c22b0631a64c1",
+    "url": "/covidtrackerappp/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "0d99ca047e2167094adf",
+    "url": "/covidtrackerappp/static/css/main.ccd29807.chunk.css"
+  },
+  {
+    "revision": "5dec0d2c22b0631a64c1",
+    "url": "/covidtrackerappp/static/js/2.b9e034c4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covidtrackerappp/static/js/2.b9e034c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d99ca047e2167094adf",
+    "url": "/covidtrackerappp/static/js/main.69b9aa3b.chunk.js"
+  },
+  {
+    "revision": "c0073596a8018fd46015",
+    "url": "/covidtrackerappp/static/js/runtime-main.a22ce20c.js"
+  }
+]);
